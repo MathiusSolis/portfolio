bb871f89c8c1750a5ab43efd2b86ddc595b58411
Mathius Solis <mathius.soliss@gmail.com>
2022-02-08 23:17:31 -0300

    feat(components-Footer): Footer component added

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import './Footer.css';
+export const Footer = () => {
+	return (
+		<footer className='footer'>
+			<div
+				className={`footer__effect footer__effect--tertiary footer__effect--left-3`}></div>
+			<div
+				className={`footer__effect footer__effect--secondary footer__effect--left-2`}></div>
+			<div
+				className={`footer__effect footer__effect--primary footer__effect--left-1`}></div>
+			<div
+				className={`footer__effect footer__effect--tertiary footer__effect--right-3`}></div>
+			<div
+				className={`footer__effect footer__effect--secondary footer__effect--right-2`}></div>
+			<div
+				className={`footer__effect footer__effect--primary footer__effect--right-1`}></div>
+			Mathius Solis - 2022
+		</footer>
+	);
+};
