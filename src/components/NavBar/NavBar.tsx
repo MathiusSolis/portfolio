f0a97227b6f75f36f387960f24d803dfe94369aa
Mathius Solis <mathius.soliss@gmail.com>
2022-02-08 22:59:34 -0300

    feat(components-NavBar): NavBar component added

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+	return (
+		<nav className='navbar'>
+			<ul className='list'>
+				<li className='list__item'>
+					<a href='#' className='list__link'>Sobre Mi</a>
+				</li>
+				<li className='list__item'>
+					<a href='#' className='list__link'>Mis Conocimientos</a>
+				</li>
+				<li className='list__item'>
+					<a href='#' className='list__link'>Mis Proyectos</a>
+				</li>
+				<li className='list__item'>
+					<a href='#' className='list__link'>Mi Curriculum</a>
+				</li>
+				<li className='list__item'>
+					<a href='#' className='list__link'>Contactame</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
