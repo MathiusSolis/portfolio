9fdf500c8a6271618dd63a734d2640d21730fb61
Mathius Solis <mathius.soliss@gmail.com>
2022-02-08 23:12:58 -0300

    feat(components-Contact): Contact component added

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,21 @@
+import { Content } from '../Content/Content';
+import phone from '../../assets/images/phone.svg';
+import email from '../../assets/images/email.svg';
+import { Title } from '../Title/Title';
+import './Contact.css';
+
+export const Contact = () => {
+	return (
+		<Content asSection={true}>
+			<Title title='Contactame' subTitle='Puedes contactarme mediante:' />
+			<div className='contact'>
+				<img className='link__img' src={phone} alt='Mi número de telefono' />
+				<a className='link' href='#'>Teléfono</a>
+			</div>
+			<div className='contact'>
+				<img className='link__img' src={email} alt='Mi Email/Correo' />
+				<a className='link' href='#'>Email/Correo</a>
+			</div>
+		</Content>
+	);
+};
