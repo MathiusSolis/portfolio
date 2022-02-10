3a25bf26f9bd70244c7fa1752f566248c607bc35
Mathius Solis <mathius.soliss@gmail.com>
2022-02-09 23:03:19 -0300

    chore: vite.config.ts config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	publicDir: 'src/assets',
 	build: {
 		lib: {
 			entry: 'src/main.tsx',
-			name: 'index',
+			name: 'index.js',
+			fileName: 'index',
 			formats: ['iife']
 		}
 	},
