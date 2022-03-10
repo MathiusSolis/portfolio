b641018be8197a480f7d2311646e7ffcbd579ab9
Mathius Solis <mathius.soliss@gmail.com>
2022-03-09 21:56:12 -0300

    fix(component-Contact): fixed anchor href path

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,8 @@
 import { Content } from '../Content/Content';
 import phone from '../../assets/images/phone.svg';
 import email from '../../assets/images/email.svg';
+import phoneNumber from '../../assets/images/phone-number.png';
+import emailCotact from '../../assets/images/email.png';
 import { Title } from '../Title/Title';
 import './Contact.css';
 
@@ -10,11 +12,11 @@ export const Contact = () => {
 			<Title title='Contactame' subTitle='Puedes contactarme mediante:' />
 			<div className='contact'>
 				<img className='link__img' src={phone} alt='Mi número de telefono' />
-				<a className='link' href='./src/assets/images/phone-number.png' download>Teléfono</a>
+				<a className='link' href={phoneNumber} download>Teléfono</a>
 			</div>
 			<div className='contact'>
 				<img className='link__img' src={email} alt='Mi Email/Correo' />
-				<a className='link' href='./src/assets/images/email.png' download>Email/Correo</a>
+				<a className='link' href={emailCotact} download>Email/Correo</a>
 			</div>
 		</Content>
 	);
