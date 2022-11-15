9335ed99f9858b0eafb1a488f78e2dd6c8f7b083
MathiusSolis <mathius.soliss@gmail.com>
2022-11-15 12:34:36 -0300

    feat(MyKnowledge-Notions): New Subcomponent added to MyKnowledge

---
+++ src/components/Sections/MyKnowledge/Notions/Notions.tsx
@@ -0,0 +1,24 @@
+import { Content } from '../../../Content/Content';
+import { LangLogo } from '../../../LangLogo/LangLogo';
+import { Title } from '../../../Title/Title';
+import javaIcon from '../../../../assets/images/Java.svg';
+import angularIcon from '../../../../assets/images/Angular.svg';
+import springIcon from '../../../../assets/images/spring.svg';
+import { BaseStyle } from '../BaseStyle/BaseStyle';
+
+export const Notions = () => {
+    return (
+        <BaseStyle>
+            <Content id="myknowledge" asSection={true}>
+                <Title title="Nociones (Aprendiendo...)" />
+                <LangLogo src={javaIcon} alt="Java Icon" des="Java" />
+                <LangLogo
+                    src={springIcon}
+                    alt="Spring Icon"
+                    des="Spring Framework"
+                />
+                <LangLogo src={angularIcon} alt="Angular Icon" des="Angular" />
+            </Content>
+        </BaseStyle>
+    );
+};
