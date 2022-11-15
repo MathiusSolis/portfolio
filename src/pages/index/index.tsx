3c206db27bfc989de335a527bd0cf59a23d20633
MathiusSolis <mathius.soliss@gmail.com>
2022-11-15 12:36:17 -0300

    fix(index): Notions component added

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,17 @@
 import { Header } from "../../components/Header/Header";
 import { AboutMe } from "../../components/Sections/AboutMe/AboutMe";
-import { MyKnowledge } from "../../components/Sections/MyKnowledge/MyKnowledge";
 import { Footer } from "../../components/Footer/Footer";
 import { MyProyects } from "../../components/Sections/MyProyects/MyProyects";
 import { Contact } from "../../components/Contact/Contact";
+import { MyKnowledge } from "../../components/Sections/MyKnowledge/BaseKnowledge/MyKnowledge";
+import { Notions } from "../../components/Sections/MyKnowledge/Notions/Notions";
 
 export const Index = () => {
     return <>
         <Header />
         <AboutMe />
         <MyKnowledge />
+        <Notions/>
         <MyProyects />
         <Contact/>
         <Footer />
