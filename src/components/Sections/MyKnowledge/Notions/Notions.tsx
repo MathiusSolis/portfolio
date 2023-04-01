4261072478b882327e4e3506af0a85eb1829f2bf
MathiusSolis <mathius.soliss@gmail.com>
2023-03-31 23:12:49 -0300

    feat(assets): Dart icon added

--- src/components/Sections/MyKnowledge/Notions/Notions.tsx
+++ src/components/Sections/MyKnowledge/Notions/Notions.tsx
@@ -16,6 +16,7 @@ export const Notions = () => {
                     alt="Spring Icon"
                     des="Spring Framework"
                 />
+                
             </Content>
         </BaseStyle>
     );
