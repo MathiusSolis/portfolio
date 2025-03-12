39b85138778fff553c6cb9a7ac6d08ff94cfb7ee
MathiusSolis <mathius.soliss@gmail.com>
2025-03-12 01:07:15 -0300

    feat(ui): Added 'isDownload' prop for download anchor in HeaderNav

--- src/app/Header/HeaderNav.tsx
+++ src/app/Header/HeaderNav.tsx
@@ -8,7 +8,7 @@ export const HeaderNav = (props: { className?: string, col?: boolean, onClick?: 
             <ul className={`h-full flex justify-center items-center ${props.col ? 'flex-col' : ''} gap-x-24 gap-y-8 flex-wrap ${FiraCode.className} text-2xl max-[1200px]:text-xl p-8 pb-0`}>
                 <HeaderNavItem onClick={props.onClick} text="Mi experiencia" href="#myexperience" />
                 <HeaderNavItem onClick={props.onClick} text="Mis proyectos" href="#myproyects" />
-                <HeaderNavItem onClick={props.onClick} text="Mi Currículum" href="#" />
+                <HeaderNavItem onClick={props.onClick} text="Mi Currículum" href="/mathius-solis-cv.pdf" isDownload={true} />
                 <HeaderNavItem onClick={props.onClick} text="Contáctame" href="#contactme" />
                 <HeaderNavItem onClick={props.onClick} text="Soporte técnico" href="#tech-support" />
             </ul>
--- src/app/Header/HeaderNavItem.tsx
+++ src/app/Header/HeaderNavItem.tsx
@@ -5,12 +5,13 @@ interface HeaderNavItemProps {
     href: string;
     className?: string;
     onClick?: MouseEventHandler<HTMLAnchorElement>;
+    isDownload?: boolean;
 }
 
 export const HeaderNavItem = (props: HeaderNavItemProps) => {
     return (
         <li className={`text-center flex items-center justify-center gap-5 border-b-4 text-white hover:border-b-[#98353c] hover:text-[#98353c] transition-colors ${props.className || ''}`}>
-            <a onClick={props.onClick} href={props.href}>{props.text}</a>
+            <a download={props.isDownload} onClick={props.onClick} href={props.href}>{props.text}</a>
         </li>
     );
 };
