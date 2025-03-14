59dff071b88da43371c653665f7a3c895edaa2cb
MathiusSolis <mathius.soliss@gmail.com>
2025-03-14 12:45:25 -0300

    feat: Updated freelance esperiences

--- src/app/Section/MyFreelanceExperience/MyFreelanceExperience.tsx
+++ src/app/Section/MyFreelanceExperience/MyFreelanceExperience.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { SectionTitle } from "../SectionTitle/SectionTitle";
 import { ProjectArticle } from "./ArticleProyect";
 import { MainBg } from "@/app/Main/MainBg";
-import bvLogo from "../../../assets/logos/bv-logo.png";
 import travelClubLogo from "../../../assets/logos/travel-club.svg";
 import toolsIcon from "../../../assets/icons/tools-icon.svg";
 
@@ -16,12 +15,6 @@ export const MyFreelanceExperience = () => {
                     <Image className="w-96 mx-auto" src={toolsIcon} alt="" />
                 </div>
                 <div className="z-10 pb-10 flex flex-wrap justify-center items-center gap-16">
-                    <ProjectArticle
-                        img={bvLogo}
-                        title="Página oficial de Buen Vivir Tecnología"
-                        description="Página WEB oficial de la empresa Buen Vivir, dedicada a la consultoría y venta de paneles solares"
-                        site="https://www.buenvivirtec.com/"
-                    />
                     <ProjectArticle
                         img={travelClubLogo}
                         title="Página miembro In Group International/Incruises"
